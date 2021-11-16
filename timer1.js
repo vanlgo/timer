eea8a3b1b6a2d099a8606f5b90023c1ca7726d42
vanlgo <goellevan@gmail.com>
2021-11-16 03:45:00 +0000

    refactored simple timer

--- timer1.js
+++ timer1.js
@@ -2,20 +2,14 @@ const beeps = process.argv.slice(2);
 
 // SOUND NOT WORKING, WRITING OUT ALERTS INSTEAD
 const timer = function(times) {
-  let alarmArr = [];
-  for (const t of times) {
-    if (t > 0) {
-      alarmArr.push(t);
+  for (let i = 0; i < times.length; i++) { 
+    if (times[i] > 0) {
+      let alertSet = times[i] * 1000;
+      setTimeout(() => {
+        process.stdout.write(`${times[i]} sec `);
+      }, alertSet);
     }
   }
-  let alertSet = alarmArr[0] * 1000;
-  for (let i = 0; i < alarmArr.length; i++) {
-    alertSet = alarmArr[i] * 1000;
-    setTimeout(() => {
-      process.stdout.write(`${alarmArr[i]} sec `);
-    }, alertSet);
-  }
 };
-
 
 timer(beeps);
