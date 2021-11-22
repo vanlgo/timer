64a2661783835f54217f3da7923f24ce4e37a157
vanlgo <goellevan@gmail.com>
2021-11-22 04:32:53 +0000

    fixed noise issue; mostly system sound problem

--- timer1.js
+++ timer1.js
@@ -1,12 +1,12 @@
 const beeps = process.argv.slice(2);
 
-// SOUND NOT WORKING, WRITING OUT ALERTS INSTEAD
 const timer = function(times) {
-  for (let i = 0; i < times.length; i++) { 
+  for (let i = 0; i < times.length; i++) {
     if (times[i] > 0) {
       let alertSet = times[i] * 1000;
       setTimeout(() => {
-        process.stdout.write(`${times[i]} sec `);
+        process.stdout.write(`${times[i]} sec `); //mostly for myself to visualize
+        process.stdout.write('\x07');
       }, alertSet);
     }
   }
